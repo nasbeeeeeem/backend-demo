4ecef51d48ef7e3c0059ddae137e6529bd6f20ef
nas <vividnasubi@gmail.com>
2023-11-04 05:12:55 +0900

    初めて書いた！^^

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,26 +10,15 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.Use(cors.New(cors.Config{
-		// アクセス許可するオリジン
-		AllowOrigins: []string{
-			"http://localhost:3000",
-		},
-		// アクセス許可するHTTPメソッド
-		AllowMethods: []string{
-			"POST",
-			"GET",
-		},
-		// 許可するHTTPリクエストヘッダ
-		AllowHeaders: []string{
-			"Authorization",
-			"Access-Token",
-		},
-		// cookieなどの情報を必要とするかどうか
-		AllowCredentials: true,
-		// preflightリクエストの結果をキャッシュする時間
-		MaxAge: 24 * time.Hour,
-	}))
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AddAllowHeaders(
+		"Authorization",
+		"Access-Token",
+	)
+	config.AllowCredentials = true
+
+	r.Use(cors.New(config))
 
 	r.GET("/hellow", func(ctx *gin.Context) {
 		token := ctx.Request.Header.Get("Access-Token")
