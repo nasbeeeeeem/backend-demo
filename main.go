13ca5f96e7a3d7f297ed3312fe9875c168387d3c
nas <vividnasubi@gmail.com>
2023-11-05 05:15:23 +0900

    feat

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"backend-demo/pkg/handler"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -10,6 +11,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
+
+type payload struct {
+	Iss            string `json:"iss"`
+	Azp            string `json:"azp"`
+	Aud            string `json:"aud"`
+	Sub            string `json:"sub"`
+	Email          string `json:"email"`
+	Email_verified bool   `json:"email_verified"`
+	At_hash        string `json:"at_hash"`
+	Name           string `json:"name"`
+	Picture        string `json:"picture"`
+	Given_name     string `json:"given_name"`
+	Family_name    string `json:"family_name"`
+	Locale         string `json:"locale"`
+	Iat            int64  `json:"iat"`
+	Exp            int64  `json:"exp"`
+}
 
 func main() {
 	r := gin.Default()
@@ -26,10 +44,10 @@ func main() {
 
 	r.Use(cors.New(config))
 
-	r.GET("/hello", func(ctx *gin.Context) {
+	r.GET("/users/me", func(ctx *gin.Context) {
 		token := ctx.Request.Header.Get("Access-Token")
 		jwt := ctx.Request.Header.Get("X-Apigateway-Api-Userinfo")
-		decStr, err := base64.StdEncoding.DecodeString(jwt)
+		dec, err := base64.StdEncoding.DecodeString(jwt)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -39,10 +57,16 @@ func main() {
 		logger.Info("auth-token",
 			zap.String("token", token),
 			zap.String("jwt", jwt),
-			zap.String("decode", string(decStr)),
+			zap.String("decode", string(dec)),
 		)
 
-		ctx.JSON(200, gin.H{"token": token})
+		var p payload
+		if err := json.Unmarshal(dec, &p); err != nil {
+			log.Fatal(err)
+		}
+
+		ctx.JSON(200, gin.H{"token": token, "name": p.Name, "email": p.Email})
+
 	})
 	r.GET("/users/me", handler.HandleMe)
 
