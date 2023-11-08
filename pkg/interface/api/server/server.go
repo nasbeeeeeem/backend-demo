c9a89b4e99bcab6623be460d3e4b921479833d5e
nas <vividnasubi@gmail.com>
2023-11-08 16:26:29 +0900

    fix: 不要なエンドポイントのコメントアウト

--- pkg/interface/api/server/server.go
+++ pkg/interface/api/server/server.go
@@ -47,43 +47,43 @@ func Server(dsn string) {
 	users := r.Group("/users")
 	{
 		// ユーザーの作成
-		users.POST("/", userHandler.HandleCreate)
+		// users.POST("/", userHandler.HandleCreate)
 		// 全ユーザの取得
-		users.GET("/", userHandler.HandleUsers)
+		// users.GET("/", userHandler.HandleUsers)
 		// 自分の取得
 		users.GET("/me", userHandler.HandleMeInfo)
 	}
 
 	// 支払い関係のエンドポイント
-	payment := r.Group("/payment")
-	{
-		// 支払い情報（現行でトップページに表示されている情報）
-		// 自分に関係ある支払い情報
-		payment.GET("/")
-		// 新規支払い登録
-		payment.POST("/")
-		// :idごとの支払い情報
-		// 参照
-		payment.GET("/:id")
-		// 修正
-		payment.POST("/:id")
-		// 削除
-		payment.DELETE("/:id")
-	}
+	// payment := r.Group("/payment")
+	// {
+	// 	// 支払い情報（現行でトップページに表示されている情報）
+	// 	// 自分に関係ある支払い情報
+	// 	payment.GET("/")
+	// 	// 新規支払い登録
+	// 	payment.POST("/")
+	// 	// :idごとの支払い情報
+	// 	// 参照
+	// 	payment.GET("/:id")
+	// 	// 修正
+	// 	payment.POST("/:id")
+	// 	// 削除
+	// 	payment.DELETE("/:id")
+	// }
 
-	// イベント関係のエンドポインおｔ
-	event := r.Group("/event")
-	{
-		// イベント一覧
-		event.GET("/")
-		// ：idごとのイベント
-		// 参照
-		event.GET("/:id")
-		// 修正
-		event.PUT("/:id")
-		// 削除
-		event.DELETE("/:id")
-	}
+	// // イベント関係のエンドポインおｔ
+	// event := r.Group("/event")
+	// {
+	// 	// イベント一覧
+	// 	event.GET("/")
+	// 	// ：idごとのイベント
+	// 	// 参照
+	// 	event.GET("/:id")
+	// 	// 修正
+	// 	event.PUT("/:id")
+	// 	// 削除
+	// 	event.DELETE("/:id")
+	// }
 
 	log.Println("Server running...")
 	if err := r.Run(":8080"); err != nil {
