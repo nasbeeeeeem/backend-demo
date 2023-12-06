8e9ad30e9335737f1ec7d731febb2ed24e567cbd
nas <vividnasubi@gmail.com>
2023-12-06 14:39:31 +0900

    fix: スキーマの修正

--- pkg/domain/model/group_user.go
+++ pkg/domain/model/group_user.go
@@ -1,8 +1,10 @@
 package model
 
 type GroupUser struct {
-	UserID        uint    `gorm:"unique"`
-	GroupID       uint    `gorm:"unique"`
-	EventsUserID  []Event `gorm:"foreignKey:CreatedBy;references:UserID"` // CreatedBy列の外部キー
-	EventsGroupID []Event `gorm:"foreignKey:GroupID;references:GroupID"`  // GroupID列の外部キー
+	UserID  uint `gorm:"uniqueIndex:idx_user_group"`
+	GroupID uint `gorm:"uniqueIndex:idx_user_group"`
+	// EventsUserID  []Event `gorm:"foreignKey:CreatedBy;references:UserID"` // CreatedBy列の外部キー
+	// EventsGroupID []Event `gorm:"foreignKey:GroupID;references:GroupID"`  // GroupID列の外部キー
+	User  User
+	Group Group
 }
